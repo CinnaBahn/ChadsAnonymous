cf48217f8caa4e018ed6acc33f21026e1308d728
waramsey <46686986+waramsey@users.noreply.github.com>
2019-06-09 12:10:33 -0500

    Background color changes.

--- Assets/killEarth.cs
+++ Assets/killEarth.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class killEarth : MonoBehaviour
 {
     public int score;
+    public Color deadSky;
+    int startScore;
+    Color startColor;
+    Camera cam;
+
+    void Start()
+    {
+        cam = GameObject.FindGameObjectsWithTag("MainCamera")[0].GetComponent<Camera>();
+        startColor = cam.backgroundColor;
+        startScore = score;
+    }
 
     void OnCollisionEnter2D(Collision2D col)
     {
         score--;
         Destroy(col.gameObject);
+        cam.backgroundColor = Color.Lerp(deadSky, startColor, (1f * score)/startScore);
     }
 }
